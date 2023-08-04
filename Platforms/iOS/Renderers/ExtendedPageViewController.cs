8e3f074668cc525eecb7f60139636833e074c9d7
Tom Soderling <18268845+TomSoderling@users.noreply.github.com>
2023-08-04 16:24:39 -0500

    Update after we got things working on iOS and Android

---
+++ Platforms/iOS/Renderers/ExtendedPageViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Maui.Platform;
+
+namespace MigratedPageRendererExample.Renderers
+{
+	public class ExtendedPageViewController : PageViewController
+    {
+        public ExtendedPageViewController(IView page, IMauiContext mauiContext) : base(page, mauiContext)
+        {
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            System.Diagnostics.Debug.WriteLine($"ExtendedPageRenderer.ViewWillAppear() called!");
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            System.Diagnostics.Debug.WriteLine($"ExtendedPageRenderer.ViewDidAppear() called!");
+        }
+    }
+}
+
